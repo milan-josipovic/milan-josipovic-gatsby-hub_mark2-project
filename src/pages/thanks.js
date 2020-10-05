0669176ccf4ccd1c50dfd8727078b78f64097492
milan-josipovic <milan.josipovic@protonmail.com>
2020-10-05 15:42:39 +0200

    thanks page added

---
+++ src/pages/thanks.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+const thanks = () => {
+  return (
+    <div>
+      Hvala. :)
+      <Link to="/">Natrag na početnu</Link>
+    </div>
+  )
+}
+
+export default thanks
