732a1e43e2e0d02c758671652f65684677a58994
milan-josipovic <milan.josipovic@protonmail.com>
2020-10-14 20:30:23 +0200

    Meetup form & CSS fix

---
+++ src/pages/meetup.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/Layout"
+import Meetup from "../components/Form/Meetup"
+
+const meetup = () => {
+  return (
+    <div>
+      <Layout>
+        <Meetup />
+      </Layout>
+    </div>
+  )
+}
+
+export default meetup
